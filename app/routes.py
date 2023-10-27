00fdeccebaba07538bfa61cc9fa6333f59b1de6c
Emili <ejanchevis@gmail.com>
2023-10-26 15:46:47 -1000

    added missing jinja code to topfive.html and created a dictionary of all items in routes for jinja in topfive

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,6 @@ def index():
 
 @app.route('/TopFive')
 def topfive():
-    return render_template('topfive.html')
+    img_ttl_txt = {"https://www.orchidsinfo.eu/wp-content/uploads/2022/05/pexels-carissa-bongalosa-1047814-scaled.jpg": ['Phalaenopsis', 'This plant originates from South-East Asia, the Philipines and Australia.'], "https://www.orchidsinfo.eu/wp-content/uploads/2022/05/raw_c6833a9494d4232b333f10785d5e5b93.jpg": ['Zygopetalum', 'Zygopetalum originates from South America.'], "https://www.orchidsinfo.eu/wp-content/uploads/2022/05/raw_b216aecdfd5b924ed514cc3317af1a52.jpg": ['Cattleya', 'Cattleyas originate from Central and South America.'], "https://www.orchidsinfo.eu/wp-content/uploads/2022/05/raw_f42c1244762dbf85c9b771cd5573ea0b.jpg": ['Cymbidium', "Most of the ancestors of today’s cymbidiums originated from the forests of the Himalayas."], "https://www.orchidsinfo.eu/wp-content/uploads/2022/05/raw_d76fd9db88827737b2fc2e8bca5782bd.jpg":['Vanda', 'The Vanda is an orchid which is widespread in nature, from India and Sri Lanka to Northern Australia.']}
+    
+    return render_template('topfive.html', img_ttl_txt=img_ttl_txt)
